7b48e035e90574d9d9071d20b428874e669ac5dd
Georges Andraus <104367200+Georges-Andraus@users.noreply.github.com>
2024-06-06 11:38:32 -0300

    Create LoginPage.vue

---
+++ LoginPage.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="login-page">
+    <div id="stars"></div>
+    <div id="stars2"></div>
+    <div id="stars3"></div>
+    <div class="section">
+      <div class="container">
+        <div class="row full-height justify-content-center">
+          <div class="col-12 text-center align-self-center py-5">
+            <div class="section pb-5 pt-5 pt-sm-2 text-center">
+              <h6 class="mb-0 pb-3">
+                <span>Log In</span>
+                <span>Sign Up</span>
+              </h6>
+              <input class="checkbox" type="checkbox" id="reg-log" name="reg-log" />
+              <label for="reg-log"></label>
+              <div class="card-3d-wrap mx-auto">
+                <div class="card-3d-wrapper">
+                  <div class="card-front">
+                    <div class="center-wrap">
+                      <div class="section text-center">
+                        <h4 class="mb-4 pb-3">Log In</h4>
+                        <LoginForm />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-back">
+                    <div class="center-wrap">
+                      <div class="section text-center">
+                        <h4 class="mb-3 pb-3">Sign Up</h4>
+                        <RegistrationForm />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue';
+import RegistrationForm from '@/components/RegistrationForm.vue';
+
+export default {
+  name: 'LoginPage',
+  components: {
+    LoginForm,
+    RegistrationForm,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
